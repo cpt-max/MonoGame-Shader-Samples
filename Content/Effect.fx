2529f74850739955a3ba573d04264572242a9fbe
markus <protonen_hoetz@gmx.at>
2020-07-20 06:05:59 +0200

    initial commit

---
+++ Content/Effect.fx
@@ -0,0 +1,186 @@
+﻿
+float4x4 WorldViewProjection;
+
+//==============================================================================
+// Vertex shader
+//==============================================================================
+struct VertexIn
+{
+	float3 Position : POSITION0;
+};
+
+struct VertexOut
+{
+	float4 Position : SV_POSITION;
+	float4 LocalPosition : TEXCOORD0;
+};
+
+VertexOut VS(in VertexIn input)
+{
+	VertexOut output;
+
+	output.LocalPosition = float4(input.Position.xyz, 1);
+	output.Position = mul(output.LocalPosition, WorldViewProjection);
+	
+	return output;
+}
+
+//==============================================================================
+// Pixel shader 
+//==============================================================================
+float4 PS(VertexOut input) : COLOR
+{
+	return float4(
+		input.LocalPosition.x * 4,
+		input.LocalPosition.y * 2,
+		input.LocalPosition.x *-4,
+		1);
+}
+
+//==============================================================================
+// Hull shader 
+//==============================================================================
+float Bend;
+float Tesselation;
+
+struct HullOut
+{
+	float4 Position : BEZIERPOS;
+};
+
+struct PatchConstantOut
+{
+	float Edges[3] : SV_TessFactor;
+	float Inside   : SV_InsideTessFactor;
+};
+
+PatchConstantOut PatchConstantFunc(
+	InputPatch<VertexOut, 3> ip,
+	uint PatchID : SV_PrimitiveID)
+{
+	PatchConstantOut output;
+
+	output.Edges[0] = Tesselation;
+	output.Edges[1] = Tesselation;
+	output.Edges[2] = Tesselation;
+
+	output.Inside = Tesselation;
+
+	return output;
+}
+
+[domain("tri")]  // tri  quad  isoline
+[partitioning("fractional_even")]  // fractional_even  fractional_odd  pow2
+[outputtopology("triangle_cw")]  // triangle_cw  triangle_ccw  line
+[outputcontrolpoints(3)]
+[patchconstantfunc("PatchConstantFunc")]
+//[maxtessfactor(2)]
+HullOut HS(InputPatch<VertexOut, 3> ip, uint i : SV_OutputControlPointID, uint patchID : SV_PrimitiveID)
+{
+	HullOut output;
+
+	output.Position = ip[i].LocalPosition;
+
+	return output;
+}
+
+//==============================================================================
+// Domain shader
+//==============================================================================
+[domain("tri")]
+VertexOut DS(const OutputPatch<HullOut, 3> patch, float3 barycentric : SV_DomainLocation, PatchConstantOut patchConst)
+{
+	VertexOut output;
+
+	float4 pos =
+		patch[0].Position * barycentric.x +
+		patch[1].Position * barycentric.y +
+		patch[2].Position * barycentric.z;
+
+	float dist = length(pos.xyz);
+	pos.z = -dist * dist * Bend;
+
+	output.Position = mul(pos, WorldViewProjection);
+	output.LocalPosition = pos;
+
+	return output;
+}
+
+//==============================================================================
+// Geometry shader 
+//==============================================================================
+float GeometryGeneration;
+
+[maxvertexcount(100)]
+void GS(triangle in VertexOut vertex[3], inout TriangleStream<VertexOut> triStream)
+{
+	float3 v0 = vertex[0].LocalPosition.xyz;
+	float3 v1 = vertex[1].LocalPosition.xyz;
+	float3 v2 = vertex[2].LocalPosition.xyz;
+
+	float size = 1 / GeometryGeneration;
+
+	for (float s = 0; s < 3; s += size)
+	{
+		float t = frac(s);
+		float3 origin = s < 1 ? lerp(v0, v1, t) :
+						s < 2 ? lerp(v1, v2, t) :
+								lerp(v2, v0, t);
+
+		vertex[0].Position = mul(float4(origin + v0 * size, 1), WorldViewProjection);
+		vertex[1].Position = mul(float4(origin + v1 * size, 1), WorldViewProjection);
+		vertex[2].Position = mul(float4(origin + v2 * size, 1), WorldViewProjection);
+		
+		triStream.Append(vertex[0]);
+		triStream.Append(vertex[1]);
+		triStream.Append(vertex[2]);
+
+		triStream.RestartStrip();
+	}
+}
+
+//==============================================================================
+// Techniques
+//==============================================================================
+technique Vertex_Pixel
+{
+	pass P0
+	{
+		VertexShader   = compile vs_4_0 VS();
+		PixelShader    = compile ps_4_0 PS();
+	}
+};
+
+technique Hull_Domain
+{
+	pass P0
+	{
+		VertexShader = compile vs_4_0 VS();
+		PixelShader = compile ps_4_0 PS();
+		HullShader = compile hs_5_0 HS();
+		DomainShader = compile ds_5_0 DS();
+	}
+};
+
+technique Geometry
+{
+	pass P0
+	{
+		VertexShader = compile vs_4_0 VS();
+		PixelShader = compile ps_4_0 PS();
+		GeometryShader = compile gs_4_0 GS();
+	}
+};
+
+technique Hull_Domain_Geometry
+{
+	pass P0
+	{
+		VertexShader = compile vs_4_0 VS();
+		PixelShader = compile ps_4_0 PS();
+		HullShader = compile hs_5_0 HS();
+		DomainShader = compile ds_5_0 DS();
+		GeometryShader = compile gs_4_0 GS();
+	}
+};
+
---
+++ Game.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ShaderTest
+{
+    public class ShaderTestGame : Game
+    {
+        const int ResolutionX = 1280;
+        const int ResolutionY = 720;
+
+        GraphicsDeviceManager graphics;
+        VertexBuffer triangle;
+        Model monkey;
+        Effect effect;
+        SpriteBatch spriteBatch;
+        SpriteFont textFont;
+
+        int technique = 3;
+        bool spaceUpLast;
+
+        float tesselation = 8;
+        float geometryGeneration = 5;
+        float bend;
+
+        public ShaderTestGame()
+        {
+            Content.RootDirectory = "Content";
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = ResolutionX;
+            graphics.PreferredBackBufferHeight = ResolutionY;
+        }
+
+        protected override void LoadContent()
+        {
+            effect = Content.Load<Effect>("Effect");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            textFont = Content.Load<SpriteFont>("TextFont");
+            triangle = CreateTriangle();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {  
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Space) && spaceUpLast)
+                technique = ++technique % 4;
+            spaceUpLast = keyboardState.IsKeyUp(Keys.Space);
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+                tesselation += dt * 5;
+            if (keyboardState.IsKeyDown(Keys.Down))
+                tesselation -= dt * 5;
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+                geometryGeneration += dt * 5;
+            if (keyboardState.IsKeyDown(Keys.Left))
+                geometryGeneration -= dt * 5;
+
+
+            tesselation = Math.Max(1, Math.Min(20, tesselation));
+            geometryGeneration = Math.Max(1, Math.Min(10, geometryGeneration));
+            bend = 4f * (float)Math.Sin(gameTime.TotalGameTime.TotalSeconds * 0.5f);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.White);
+
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+
+            Matrix world = Matrix.CreateScale(1);
+            Matrix view = Matrix.CreateLookAt(new Vector3(0, -1, 5), new Vector3(0, 1, 0), new Vector3(0, 1, 0));
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), (float)ResolutionX / (float)ResolutionY, 0.1f, 1000f);
+   
+            effect.CurrentTechnique = effect.Techniques[technique];
+            effect.Parameters["WorldViewProjection"].SetValue(world * view * projection);
+            effect.Parameters["Bend"].SetValue(bend);
+            effect.Parameters["Tesselation"].SetValue(tesselation);
+            effect.Parameters["GeometryGeneration"].SetValue(geometryGeneration);     
+
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                PrimitiveType primitiveType = hullShaderActive ?
+                    PrimitiveType.PatchListWith3ControlPoints :
+                    PrimitiveType.TriangleList;
+
+                GraphicsDevice.SetVertexBuffer(triangle);
+                GraphicsDevice.DrawPrimitives(primitiveType, 0, triangle.VertexCount / 3);
+            }
+
+            DrawText();
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawText()
+        {
+            string text = "Space to change Shader Stages: \n";
+            text += "Arrow Up/Down to change Tesselation: \n";
+            text += "Arrow Left/Right to change GeometryGeneration: \n";
+
+            string values = effect.CurrentTechnique.Name + "\n";
+            values += (hullShaderActive ? tesselation.ToString() : "") + "\n";
+            values += (geometryShaderActive ? geometryGeneration.ToString() : "") + "\n";
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(textFont, text, new Vector2(50, 50), Color.Black);
+            spriteBatch.DrawString(textFont, values, new Vector2(800, 50), Color.Black);
+            spriteBatch.End();
+        }
+
+        private VertexBuffer CreateTriangle()
+        {
+            var vertices = new VertexPosition[] {
+                new VertexPosition(new Vector3( 0, 1, 0)),
+                new VertexPosition(new Vector3( 1, 0, 0)),
+                new VertexPosition(new Vector3(-1, 0, 0)),
+            };
+
+            var vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPosition), vertices.Length, BufferUsage.WriteOnly);
+            vertexBuffer.SetData(vertices);
+            return vertexBuffer;
+        }
+
+        private bool hullShaderActive => effect.CurrentTechnique.Name.Contains("Hull");
+        private bool geometryShaderActive => effect.CurrentTechnique.Name.Contains("Geometry");
+    }
+}
