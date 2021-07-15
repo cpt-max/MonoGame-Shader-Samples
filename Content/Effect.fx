0c28cc93760f93a9f76a265f6286fb0bdcb2b9da
markus <protonen_hoetz@gmx.at>
2021-07-15 19:18:00 +0200

    compute shader particles

--- Content/Effect.fx
+++ Content/Effect.fx
@@ -1,52 +1,91 @@
-﻿struct Input
+﻿struct Particle
 {
     float2 pos;
+    float2 vel;
 };
 
-struct Output
-{
-    int collisions;
-};
-
-StructuredBuffer<Input> Inputs;
-RWStructuredBuffer<Output> Outputs;
-
-int ObjectCount;
-float ObjectSize;
-
-//================================================================================================
+//=============================================================================
 // Compute Shader
-//================================================================================================
-#define GroupSize 10
+//=============================================================================
+#define ComputeGroupSize 256
 
-[numthreads(GroupSize, 1, 1)]
+RWStructuredBuffer<Particle> Particles;
+
+float DeltaTime;
+float Force;
+float2 ForceCenter;
+
+[numthreads(ComputeGroupSize, 1, 1)]
 void CS(uint3 localID : SV_GroupThreadID, uint3 dispatchID : SV_GroupID,
 	    uint  localIndex : SV_GroupIndex, uint3 globalID : SV_DispatchThreadID)
 {
-    float2 pos = Inputs[globalID.x].pos;
-    int collisions = 0;
+    Particle p = Particles[globalID.x];
     
-    for (int i = 0; i < ObjectCount; i++)
-    {
-        if ((uint)i == globalID.x)
-            continue;
-        
-        float2 posOther = Inputs[i].pos;
-        float dist = distance(pos, posOther);
-        if (dist < ObjectSize)
-            collisions++;
-    }
+    float2 toCenter = ForceCenter - p.pos;
+    float distSqr = dot(toCenter, toCenter);
     
-    Outputs[globalID.x].collisions = collisions;
+    p.vel += (Force * DeltaTime / distSqr) * normalize(toCenter); // apply force
+    p.vel *= max(0.1, 1 - DeltaTime * dot(p.vel, p.vel) * 100); // velocity damping
+    
+    p.pos += p.vel * DeltaTime; // move
+    p.pos -= (p.pos >  1) * 2; // wrap on border
+    p.pos += (p.pos < -1) * 2; // wrap on borders
+    
+    Particles[globalID.x] = p;
 }
 
-//================================================================================================
+//==============================================================================
+// Vertex shader
+//==============================================================================
+StructuredBuffer<Particle> ParticlesReadOnly;
+
+struct VertexIn
+{
+    float3 Position : POSITION0;
+    float2 TexCoord : TEXCOORD0;
+    uint VertexID : SV_VertexID;
+};
+
+struct VertexOut
+{
+    float4 Position : SV_POSITION;
+    float2 TexCoord : TEXCOORD0;
+};
+
+VertexOut VS(in VertexIn input)
+{
+    VertexOut output;
+    
+    uint particleID = input.VertexID / 4;
+    Particle p = ParticlesReadOnly[particleID];
+    
+    float2 size = float2(1, 16.0 / 9.0) * 0.001;
+    float2 pos = p.pos + input.Position.xy * size;
+
+    output.Position = float4(pos, 0, 1);
+    output.TexCoord = input.TexCoord;
+	
+    return output;
+}
+
+//==============================================================================
+// Pixel shader 
+//==============================================================================
+float4 PS(VertexOut input) : SV_TARGET
+{
+    return float4(0.5, 0.8, 1, 1);
+}
+
+
+//===============================================================================
 // Techniques
-//================================================================================================
+//===============================================================================
 technique Tech0
 {
     pass Pass0
     {
+        VertexShader = compile vs_5_0 VS();
+        PixelShader = compile ps_4_0 PS();
         ComputeShader = compile cs_5_0 CS();
     }
 }
--- Game.cs
+++ Game.cs
@@ -5,14 +5,10 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ShaderTest
 {
-    struct Circle
+    struct Particle
     {
         public Vector2 pos;
-    };
-
-    struct Collision
-    {
-        public int collisionCount;
+        public Vector2 vel;
     };
 
     public class ShaderTestGame : Game
@@ -20,26 +16,26 @@ namespace ShaderTest
         const int ResolutionX = 1280;
         const int ResolutionY = 720;
 
-        const int ComputeGroupSize = 10;
-        const int MaxCircleCount = 10000;
+        const int MaxParticleCount = 1000000;
+        const int ComputeGroupSize = 256; // has to be the same as the ComputeGroupSize define in the compute shader 
 
-        int circleCount { get { return (int)circleCountFloat; } }
-        float circleCountFloat = 50;
-        float circleSize = 80;
-
-        Circle[] circles = new Circle[MaxCircleCount];
-        Collision[] collisions = new Collision[MaxCircleCount];
-
-        StructuredBuffer circlesBuffer;
-        StructuredBuffer collisionsBuffer;
+        int particleCount = 10000;
+        float fps;
+        bool spacePressed;
+        float force;
+        Vector2 forceCenter;
 
         GraphicsDeviceManager graphics;
+        Random rand = new Random();
+
         Effect effect;
         Texture2D texture;
         SpriteBatch spriteBatch;
         SpriteFont textFont;
 
-        Random rand = new Random();
+        StructuredBuffer particleBuffer; // stores all the particle information, will be updated by the compute shader
+        VertexBuffer quadVertexBuffer; // used for drawing the particles
+        IndexBuffer quadIndexBuffer;   // used for drawing the particles
 
         public ShaderTestGame()
         {
@@ -65,45 +61,41 @@ namespace ShaderTest
             texture = Content.Load<Texture2D>("Texture");
             textFont = Content.Load<SpriteFont>("TextFont");
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            particleBuffer = new StructuredBuffer(GraphicsDevice, typeof(Particle), MaxParticleCount, BufferUsage.None, true);
 
-            circlesBuffer = new StructuredBuffer(GraphicsDevice, typeof(Circle), MaxCircleCount, BufferUsage.None, false);
-            collisionsBuffer = new StructuredBuffer(GraphicsDevice, typeof(Collision), MaxCircleCount, BufferUsage.None, true);
-
-            FillBufferWithRandomCircles();
-        }
-
-        private void FillBufferWithRandomCircles()
-        {
-            for (int i = 0; i < MaxCircleCount; i++)
-            {
-                circles[i].pos = new Vector2(
-                    (float)rand.NextDouble() * ResolutionX,
-                    (float)rand.NextDouble() * ResolutionY);
-            }
-
-            circlesBuffer.SetData(circles);
+            FillParticlesBufferRandomly();
+            CreateQuadBufferForParticleRendering(ref quadVertexBuffer, ref quadIndexBuffer);
         }
 
         protected override void Update(GameTime gameTime)
         {
             float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            KeyboardState keyboardState = Keyboard.GetState();
+            fps = 1 / dt;
+
+            var keyboardState = Keyboard.GetState();
+            var mouseState = Mouse.GetState();
 
             if (keyboardState.IsKeyDown(Keys.W))
-                circleSize += dt * circleSize;
+                particleCount += (int)Math.Ceiling(dt * particleCount);
             if (keyboardState.IsKeyDown(Keys.Q))
-                circleSize -= dt * circleSize;
+                particleCount -= (int)Math.Ceiling(dt * particleCount);
 
-            if (keyboardState.IsKeyDown(Keys.S))
-                circleCountFloat += dt * circleCountFloat;
-            if (keyboardState.IsKeyDown(Keys.A))
-                circleCountFloat -= dt * circleCountFloat;
-
-            circleSize = Math.Max(5, Math.Min(300, circleSize));
-            circleCountFloat = Math.Max(1, Math.Min(MaxCircleCount, circleCountFloat));      
+            particleCount = Math.Max(1, Math.Min(MaxParticleCount, particleCount));
 
             if (keyboardState.IsKeyDown(Keys.Space))
-                FillBufferWithRandomCircles();
+            {
+                if (!spacePressed)
+                    FillParticlesBufferRandomly();
+                spacePressed = true;
+            } 
+            else spacePressed = false;
+
+            force = mouseState.LeftButton  == ButtonState.Pressed ?  1f :
+                    mouseState.RightButton == ButtonState.Pressed ? -1f : 0f;
+            force *= 5;
+
+            forceCenter = mouseState.Position.ToVector2() / new Vector2(ResolutionX, ResolutionY) * 2 - Vector2.One;
+            forceCenter.Y *= -1;
 
             base.Update(gameTime);
         }
@@ -112,72 +104,118 @@ namespace ShaderTest
         {
             graphics.GraphicsDevice.Clear(Color.Black);
 
-            RunCompute();
-            DrawCircles();
+            ComputeParticles(gameTime);
+
+            DrawParticles();
             DrawText();
+            DrawMousePointer();
 
             base.Draw(gameTime);
         }
 
-        private void RunCompute()
+        private void ComputeParticles(GameTime gameTime)
         {
-            effect.Parameters["ObjectCount"].SetValue(circleCount);
-            effect.Parameters["ObjectSize"].SetValue(circleSize);
-            effect.Parameters["Inputs"].SetValue(circlesBuffer);
-            effect.Parameters["Outputs"].SetValue(collisionsBuffer);
+            effect.Parameters["Particles"].SetValue(particleBuffer);
+            effect.Parameters["DeltaTime"].SetValue((float)gameTime.ElapsedGameTime.TotalSeconds);
+            effect.Parameters["ForceCenter"].SetValue(forceCenter);
+            effect.Parameters["Force"].SetValue(force);
+
+            int groupCount = (int)Math.Ceiling((double)particleCount / ComputeGroupSize);
 
             foreach (var pass in effect.CurrentTechnique.Passes)
             {
                 pass.ApplyCompute();
-
-                int dispatchCount = (int)Math.Ceiling((double)circleCount / ComputeGroupSize);
-                GraphicsDevice.DispatchCompute(dispatchCount, 1, 1);
+                GraphicsDevice.DispatchCompute(groupCount, 1, 1);
             }
-
-            collisionsBuffer.GetData(collisions, 0, circleCount);
         }
 
-        private void DrawCircles()
+        private void DrawParticles()
         {
-            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearClamp);
+            effect.Parameters["ParticlesReadOnly"].SetValue(particleBuffer);
 
-            for (int i = 0; i < circleCount; i++)
+            foreach (var pass in effect.CurrentTechnique.Passes)
             {
-                Point size = new Point((int)circleSize);
-                Point radius = new Point((int)(circleSize/2));
-                Point pos = circles[i].pos.ToPoint() - radius;
-                
-                Color col;
-                switch(collisions[i].collisionCount)
-                {
-                    case 0: col = new Color(255, 255, 255); break;
-                    case 1: col = new Color(86,255,0); break;
-                    case 2: col = new Color(255, 80, 235); break;
-                    case 3: col = new Color(255, 231, 0); break;
-                    default: col = new Color(0, 175, 255); break;
-                }
+                pass.Apply();
 
-                spriteBatch.Draw(texture, new Rectangle(pos, size), null, col);
+                GraphicsDevice.SetVertexBuffer(quadVertexBuffer);
+                GraphicsDevice.Indices = quadIndexBuffer;
+                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, particleCount * 2);
             }
-                
-            spriteBatch.End(); 
+        }
+
+        private void DrawMousePointer()
+        {
+            Vector2 posPixel = (forceCenter * new Vector2(1, -1) + Vector2.One) / 2 * new Vector2(ResolutionX, ResolutionY);
+            Vector2 origin = new Vector2(texture.Width, texture.Height) / 2;
+            Vector2 size = Vector2.One * 0.05f;
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, posPixel, null, new Color(255,230,50), 0, origin, size, SpriteEffects.None, 0);
+            spriteBatch.End();
         }
 
         private void DrawText()
         {
-            string text = "A and S for Circle Size: \n"; 
-            text +=       "Q and W for Circle Count: \n";
-            text +=       "Collision Checks: \n";
+            string text = "FPS\n";
+            text +=       "Q and W for Particle Count\n";
+            text +=       "Mouse for Force Fields\n";
             text +=       "Space for Randomize\n";
 
-            string values = circleSize.ToString("0") + "\n";
-            values += circleCount.ToString() + "\n";
-            values += (circleCount * circleCount).ToString() + "\n";
+            string values = fps.ToString("0") + "\n";
+            values += particleCount.ToString() + "\n";
 
             spriteBatch.Begin();
             spriteBatch.DrawString(textFont, text, new Vector2(30, 30), Color.White);
             spriteBatch.DrawString(textFont, values, new Vector2(1050, 30), Color.White);
             spriteBatch.End();
+        }
+
+        private void FillParticlesBufferRandomly()
+        {
+            Particle[] particles = new Particle[MaxParticleCount];
+
+            for (int i = 0; i < MaxParticleCount; i++)
+            {
+                particles[i].pos = new Vector2(
+                    (float)rand.NextDouble() * 2 - 1,
+                    (float)rand.NextDouble() * 2 - 1);
+
+                particles[i].vel = new Vector2(
+                    (float)(rand.NextDouble() * 2 - 1) * 0.1f,
+                    (float)(rand.NextDouble() * 2 - 1) * 0.1f);
+            }
+
+            particleBuffer.SetData(particles);
+        }
+
+        private void CreateQuadBufferForParticleRendering(ref VertexBuffer quadVertexBuffer, ref IndexBuffer quadIndexBuffer)
+        {
+            var vertices = new VertexPositionTexture[MaxParticleCount * 4];
+            var indices = new int[MaxParticleCount * 6];
+
+            for (int i = 0; i < MaxParticleCount; i++)
+            {
+                int vInd = i * 4;
+                int iInd = i * 6;
+
+                vertices[vInd + 0] = new VertexPositionTexture(new Vector3(-1,  1, 0), new Vector2(0, 0));
+                vertices[vInd + 1] = new VertexPositionTexture(new Vector3( 1,  1, 0), new Vector2(1, 0));
+                vertices[vInd + 2] = new VertexPositionTexture(new Vector3( 1, -1, 0), new Vector2(1, 1));
+                vertices[vInd + 3] = new VertexPositionTexture(new Vector3(-1, -1, 0), new Vector2(0, 1));
+
+                indices[iInd + 0] = vInd + 0;
+                indices[iInd + 1] = vInd + 1;
+                indices[iInd + 2] = vInd + 2;
+                indices[iInd + 3] = vInd + 0;
+                indices[iInd + 4] = vInd + 2;
+                indices[iInd + 5] = vInd + 3;
+            };
+
+            quadVertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionTexture), vertices.Length, BufferUsage.WriteOnly);
+            quadVertexBuffer.SetData(vertices);
+
+            quadIndexBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.ThirtyTwoBits, indices.Length, BufferUsage.WriteOnly);
+            quadIndexBuffer.SetData(indices);
         }
     }
 }
