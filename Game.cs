96508d9c7d4956b172a3f851d289e1fab029f7f5
markus <protonen_hoetz@gmx.at>
2021-12-12 07:24:36 +0100

    update to newest nuget version

--- Game.cs
+++ Game.cs
@@ -38,10 +38,11 @@ namespace ShaderSample
 
         protected override void Initialize()
         {
-            base.Initialize();
             graphics.PreferredBackBufferWidth = ResolutionX;
             graphics.PreferredBackBufferHeight = ResolutionY;
             graphics.ApplyChanges();
+
+            base.Initialize();
         }
 
         protected override void LoadContent()
